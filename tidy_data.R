d8dc9029098f00126a1c1f3f7b8a09e4281fb9a3
Vincent <Vincent Toups>
2020-08-30 13:16:52 -0400

    Pivoted the powers data into row-wise data.

--- tidy_data.R
+++ tidy_data.R
@@ -1,8 +1,23 @@
 library(tidyverse);
+source("utils.R");
+
+`%without%` <- function(a, b){
+    a[!(a %in% b)];
+}
 
 information <- 
   read_csv("source_data/datasets_26532_33799_heroes_information.csv");
 powers <- read_csv("source_data/datasets_26532_33799_super_hero_powers.csv");
+
+powers <- tidy_up_names(powers);
+information <- tidy_up_names(information);
+
+powers <- powers %>%
+    rename(name=hero_names);
+
+powers <- powers  %>%
+    pivot_longer(cols=names(powers) %without% "name",
+                 names_to="power",values_to="has");
 
 # Do something to clean stuff up.
 
